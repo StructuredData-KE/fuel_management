35c736b028b276fbe69c6fe546c701f4214c7e40
USER <user@example.com>
2026-07-23 14:49:11 +0300

    Fix python syntax in Station Purchase

--- fuel_management/fuel_management/doctype/station_purchase/station_purchase.py
+++ fuel_management/fuel_management/doctype/station_purchase/station_purchase.py
@@ -6,45 +6,10 @@ class StationPurchase(Document):
     def validate(self):
         self.total_cost = self.quantity * self.unit_cost
         if self.vat_inclusive:
-            self.net_total = self.total_cost
-
-    def on_submit(self):
-        # We will create a Purchase Receipt immediately
-        # But wait, Station Purchase doesn't have submit, it's just inserted.
-        pass
-
-    def after_insert(self):
-        self.create_purchase_receipt()
-
-    def create_purchase_receipt(self):
-        pr = frappe.new_doc("Purchase Receipt")
-        pr.supplier = self.supplier
-        pr.posting_date = self.document_date or self.receiving_date or frappe.utils.nowdate()
-        pr.set_posting_time = 1
-        
-        # We assume target_location is a Warehouse
-        pr.append("items", {
-            "item_code": self.item,
-            "qty": self.quantity,
-            "rate": self.unit_cost,
-            "warehouse": self.target_location,
-            "received_qty": self.quantity
-        })
-        
-        pr.flags.ignore_permissions = True
-        pr.insert()
-        pr.submit()
-        
-        frappe.msgprint(f"Generated Purchase Receipt {pr.name} for {self.item}")
- / 1.16  # Assuming 16% VAT standard in KE
+            self.net_total = self.total_cost / 1.16  # Assuming 16% VAT standard in KE
         else:
             self.net_total = self.total_cost
 
-    def on_submit(self):
-        # We will create a Purchase Receipt immediately
-        # But wait, Station Purchase doesn't have submit, it's just inserted.
-        pass
-
     def after_insert(self):
         self.create_purchase_receipt()
 
@@ -68,4 +33,3 @@ class StationPurchase(Document):
         pr.submit()
         
         frappe.msgprint(f"Generated Purchase Receipt {pr.name} for {self.item}")
-
