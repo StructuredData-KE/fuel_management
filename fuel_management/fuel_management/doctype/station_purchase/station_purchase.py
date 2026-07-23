2e7905bb125c0c4704222faad45bc64e8afdf292
USER <user@example.com>
2026-07-23 14:51:39 +0300

    Switch Purchase Receipt to Purchase Invoice with update_stock

--- fuel_management/fuel_management/doctype/station_purchase/station_purchase.py
+++ fuel_management/fuel_management/doctype/station_purchase/station_purchase.py
@@ -11,25 +11,28 @@ class StationPurchase(Document):
             self.net_total = self.total_cost
 
     def after_insert(self):
-        self.create_purchase_receipt()
+        self.create_purchase_invoice()
 
-    def create_purchase_receipt(self):
-        pr = frappe.new_doc("Purchase Receipt")
-        pr.supplier = self.supplier
-        pr.posting_date = self.document_date or self.receiving_date or frappe.utils.nowdate()
-        pr.set_posting_time = 1
+    def create_purchase_invoice(self):
+        pi = frappe.new_doc("Purchase Invoice")
+        pi.supplier = self.supplier
+        pi.posting_date = self.document_date or self.receiving_date or frappe.utils.nowdate()
+        pi.posting_time = frappe.utils.nowtime()
+        pi.set_posting_time = 1
+        pi.update_stock = 1 # CRITICAL: Update physical inventory tanks/stores
         
         # We assume target_location is a Warehouse
-        pr.append("items", {
+        pi.append("items", {
             "item_code": self.item,
             "qty": self.quantity,
             "rate": self.unit_cost,
             "warehouse": self.target_location,
-            "received_qty": self.quantity
+            "received_qty": self.quantity,
+            "expense_account": frappe.get_cached_value("Company", frappe.defaults.get_user_default("Company"), "default_expense_account") or "Cost of Goods Sold"
         })
         
-        pr.flags.ignore_permissions = True
-        pr.insert()
-        pr.submit()
+        pi.flags.ignore_permissions = True
+        pi.insert()
+        pi.submit()
         
-        frappe.msgprint(f"Generated Purchase Receipt {pr.name} for {self.item}")
+        frappe.msgprint(f"Generated Purchase Invoice {pi.name} for {self.item}")
