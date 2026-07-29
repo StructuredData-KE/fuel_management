41a1199adcee9b78cd56992f6a823ebfd4a495f6
USER <user@example.com>
2026-07-29 11:34:50 +0300

    chore: add debug script

---
+++ fuel_management/fuel_management/debug_modules.py
@@ -0,0 +1,8 @@
+﻿import frappe
+def execute():
+    try:
+        print("Installed apps:", frappe.get_installed_apps())
+        print("Module list for fuel_management:", frappe.get_module_list("fuel_management"))
+        print("Module app cache:", frappe.local.module_app.get("fuel_management"))
+    except Exception as e:
+        print(e)
