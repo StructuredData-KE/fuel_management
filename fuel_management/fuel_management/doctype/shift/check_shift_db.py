1a039193f148dba65230bee5d1bc8a98cfca2c9e
USER <user@example.com>
2026-08-03 08:39:45 +0300

    temp

--- fuel_management/fuel_management/doctype/shift/check_shift_db.py
+++ fuel_management/fuel_management/doctype/shift/check_shift_db.py
@@ -1,6 +1,6 @@
 import frappe
 
 def check_shifts():
-    shifts = frappe.db.sql("SELECT name, status, start_time FROM \	abShift\ ORDER BY modified DESC LIMIT 5;", as_dict=True)
+    shifts = frappe.db.sql("SELECT name, status, start_time FROM tabShift ORDER BY modified DESC LIMIT 5;", as_dict=True)
     for s in shifts:
         print(s)
